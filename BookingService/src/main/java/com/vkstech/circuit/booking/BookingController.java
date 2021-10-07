b94e111723cdec448df815d5cf702bdca1841350
Vipul Kumar Singh <vipul24.9669@gmail.com>
2021-10-07 23:35:36 +0530

    Implemented Hystrix

--- BookingService/src/main/java/com/vkstech/circuit/booking/BookingController.java
+++ BookingService/src/main/java/com/vkstech/circuit/booking/BookingController.java
@@ -1,6 +1,8 @@
 package com.vkstech.circuit.booking;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,17 +10,32 @@ import org.springframework.web.client.RestTemplate;
 
 @RestController
 @RequestMapping("booking")
+@EnableHystrix
 public class BookingController {
 
     @Autowired
     private RestTemplate template;
 
-    @GetMapping("send")
+    @GetMapping("bookNow")
+    @HystrixCommand(groupKey = "vkstech", commandKey = "vkstech", fallbackMethod = "bookShowFallback")
     public String bookShow() {
 
-        String emailServiceResponse = template.getForObject("http://localhost:8081/email/send", String.class);
-        String paymentServiceResponse = template.getForObject("https://localhost:8082/payment/pay", String.class);
+        String emailServiceResponse = template.getForObject("http://localhost:8082/email/send", String.class);
+        String paymentServiceResponse = template.getForObject("http://localhost:8083/payment/pay", String.class);
 
         return emailServiceResponse + "\n" + paymentServiceResponse;
+    }
+
+    @GetMapping("bookNowWithoutHystrix")
+    public String bookShowWithoutHystrix() {
+
+        String emailServiceResponse = template.getForObject("http://localhost:8082/email/send", String.class);
+        String paymentServiceResponse = template.getForObject("http://localhost:8083/payment/pay", String.class);
+
+        return emailServiceResponse + "\n" + paymentServiceResponse;
+    }
+
+    public String bookShowFallback() {
+        return "service gateway failed";
     }
 }
